c45334dd4eed84bdeafd62ba1b7e909f14d8e743
Elvis <s49188705@gmail.com>
2022-11-21 18:31:22 +0300

    Rails_Route_Params_lab

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,14 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def grades 
+    students = Student.order(grade: :desc)
+    render json: students
+  end
+
+  def  show 
+    students = Student.find(params[:id])
+    render json: students
+  end
+
 end
